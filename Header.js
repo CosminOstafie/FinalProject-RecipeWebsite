05d1eb96a75a92f7e89862ededc5ef634c3ec5be
CosminOstafie <116910376+CosminOstafie@users.noreply.github.com>
2022-12-01 13:31:20 +0100

    header

---
+++ Header.js
@@ -0,0 +1,14 @@
+const togglebtn = document.getElementsByClassName("togglebtn")[0];
+let menus = document.getElementsByClassName("menus")[0];
+const searchBar = document.getElementsByClassName("search")[0];
+console.log("merge pana aici");
+togglebtn.addEventListener("click", function () {
+  menus.classList.toggle("active");
+  if (menus.style.display === "flex" && searchBar.style.display === "flex") {
+    menus.style.display = "none";
+    searchBar.style.display = "none";
+  } else {
+    menus.style.display = "flex";
+    searchBar.style.display = "flex";
+  }
+});
